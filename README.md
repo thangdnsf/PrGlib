3c4e901d1d39f710db1d466c4274c89b12279913
thang19xx <thang19xx@hotmail.com>
2015-07-18 10:58:16 +0700

    the first commit

---
+++ README.md
@@ -0,0 +1,116 @@
+﻿
+
+PHƯƠNG PHÁP SINH KHÓA VÀ MÃ HÓA RSA
+
+Thangdn – 18.7.2015
+
+
+I.PHƯƠNG PHÁP SINH KHÓA
+
+Theo định lý về số nguyên tố, thì khoảng cách trung bình của hai số nguyên tố lớn cỡ nbit khoảng n*ln2. Vậy trong khoảng này có khản năng cao sẽ tồn tại số nguyên tố.
+
+1.Sinh số ngẫu nhiên
+
+Ta cần tạo ra số giả ngẫu nhiên n kích thước đủ lớn cơ 3072bit. Vì được sử dụng trong mật mã nên số giả ngẫu nhiên phải đảm bảo không đoán được, không trùng nhau và cách xa nhau.
+
+Trong chương trình này sử dụng hàm RandomBits()(thư viện NTL) để sinh một chuỗi ngẫu nhiên n bit đảm bảo an toàn.
+
+2.Tiền xử lý
+
+Ta sẽ xét trong khoảng hơn 2*n*ln2 số bằng thuật toán Rabin-miller, ta có thể loại nhanh hơn các hợp số với 1000 số nguyên tố nhỏ. Qua bước xử lý này có thể loại bỏ 93% hợp số trong khoảng [n,n+2*n*ln2).
+
+Các bước thực hiện:
+
+- Tạo danh sách 1000 số nguyên tố đầu tiên.
+
+- Với mỗi số ngẫu nhiên n, tạo một mảng bít S kích thước 2*n*ln2, khởi tạo bằng 0.
+
+- Với mỗi số nguyên tố p khởi tạo ban đầu, tính r=n%p. Gán tất cả các phần tử S[p*k-r]=1. Ta thấy rằng n+p*k-r luôn chia hết cho p. Như vậy những phần tử S[i]=1, ta luôn có n+i chia hết cho p.
+
+Sau khi kết thúc ta sẽ chỉ cần kiểm tra các phần tử n+i với S[i]=0.
+
+3.Kiểm tra số giả nguyên tố Fermat cơ sở 2
+
+Việc kiểm tra số giả nguyên tố fermat cơ sở 2 sẽ giúp loại được nhiều hợp số.
+
+4.Kiểm tra Rabin-miller
+
+Với sai số của số giả nguyên tố là 2-128, vậy nên ở đây ta sẽ thực hiện kiểm tra Rabin-miller với 128/2 cơ số khác nhau cho n, bao gồm 20 số nguyên tố đầu tiên và 44 số sinh ngẫy nhiên <= căn bậc 2 của n.
+
+5.Sinh số nguyên tố mạnh
+
+Với việc sinh số nguyên tố mạnh đảm bảo tính khó tấn công bằng việc phân tích nguyên tố n=p*q đối với p,q là số nguyên tố yếu. Số nguyên tố mạnh phải đảm bảo những yêu cầu sau:
+
+- Số p là một số cực lớn
+
+- Thừa số nguyên tố lớn nhất của p-1 là p- một số lớn.
+
+sao cho p=a-*p- +1 là số nguyên tố nhỏ nhất với a- tự chọn
+
+- Thừa số nguyên tố lớn nhất của p- - 1 là p-- một số lớn.
+
+sao cho p-=a--*p-- + 1 là số nguyên tố nhỏ nhất với a-- tự chọn
+
+Thừa số nguyên tố lớn nhất của p + 1 là p+ là số nguyên tố
+
+sao cho p=a+*p+ -1 là số nguyên tố với a+ là tự chọn
+
+Ta sử dụng thuật toán của Gordon's cho việc tìm số nguyên tố mạnh
+
+1. Tìm hai số p-- và p+ là hai số ngẫu nhiên lớn cỡ n/2bit (nbit là độ dài số nguyên tố mạnh cần tìm). Chú ý hai số này phải khác nhau hoàn toàn.
+
+2. Tìm p- là số nguyên tố nhỏ nhất theo công thức p-=a-*p--+1
+
+3. Tính po=((p+)p- -1- (p-)p+ -1)mod (p-*p+).
+
+4. Tìm p là số nguyên tố nhỏ nhất theo công thức p=po+a*p-*p+
+
+6.Thời gian chạy
+
+Qua quá trình cài đặt thuật toán bằng ngôn ngữ C++ trên hệ điều thành Ubuntu thấy tố độ như sau.
+
+Sinh số nguyên tố độ dài 3072bit trong khoảng thời gian là 0-2s.
+
+Sinh số nguyên tố mạnh độ dài 3072bit trong khoảng thời gian là 1-4s
+
+Yêu cầu sử dụng thư viện NTL để sinh số ngẫu nhiên an toàn, và tính toán bằng thư viện GMP
+
+II.MÃ HÓA RSA
+
+1.Sinh khóa
+
+Sinh hai ngẫu nhiên hai số p và q cách xa nhau là hai số nguyên tố mạnh cơ 3072bit
+
+Tính n=p*q
+
+Tính phi(n)=(p-1)*(q-1).
+
+Chọn e sao cho gcd(e, phi(n))=1
+
+Tính d = e-1 mod phi(n)
+
+Private key (n,d) Public key (n,e)
+
+2.Mã hóa
+
+1. Mã hóa một chuỗi số có độ dài thuộc KEY{128,192,256}
+
+2. Sinh chuỗi 2048 bit trong đó 16bit đầu là bit mặc định KEY là số bit đặt cuối, 2032-KEY là số bit giữa sẽ random.
+
+3. Mã hóa chuỗi số 2048bit trên bởi công thức C=Me mod n
+
+4. Lưu C dưới dạng base64 tức gom 6bit một để tránh các ký tự điều khiển.
+
+3.Giải mã
+
+Quy trình giải mã làm ngược lại quá trình mã hóa với M=Cd mode n
+
+
+TÀI LIỆU THAM KHẢO:
+
+[1]. Are `Strong' Primes Needed for RSA?,Ronald L. Rivest #,Robert D. Silverman y
+
+November 22, 1999
+
+[2]. Strong primes are easy to find, John Gordon, Cybermation L t d
+
---
+++ RSA.cpp
@@ -0,0 +1,172 @@
+#include "RSA.h"
+
+RSA::RSA()
+{
+    //ctor
+}
+
+void RSA::createNewKey(char *filePrivateKey,char * filePublicKey)
+{
+    PrGlib dnthang;
+    ofstream PK(filePrivateKey);
+    ofstream BK(filePublicKey);
+    /*Generate p and q as strong primes */
+    ZZZ p=dnthang.PrG_generate_strong_prime(3072);
+
+    ZZZ q=dnthang.PrG_generate_strong_prime(3072);
+    ZZZ n=q*p;
+    ZZZ phi=(p^1)*(q^1);
+    ZZZ e,k;
+    /*Find e such that gcd(e,phi)=1*/
+    do
+    {
+        gmp_randclass rr(gmp_randinit_default);
+		rr.seed(time(NULL));
+        e =rr.get_z_bits(dnthang.PrG_get_length());
+		ZZZ num = e & 1;
+		if (num == 0)e = e | 1;
+        mpz_gcd (k.get_mpz_t(),e.get_mpz_t(), phi.get_mpz_t());
+    }while(k!=1);
+    /*******************************/
+    /*Compute d= e^-1 mod n*/
+    ZZZ d;
+    mpz_invert(d.get_mpz_t(),e.get_mpz_t(),phi.get_mpz_t());
+    /*private key*/
+    PK<<n.get_str()<<endl;
+    PK<<d.get_str()<<endl;
+    /*public key*/
+    BK<<n.get_str()<<endl;
+    BK<<e.get_str()<<endl;
+    PK.clear();
+    PK.close();
+    BK.clear();
+    BK.close();
+}
+void RSA::decryptRSA (char* fileText,char* fileKey,char *fileDecrypt)
+{
+    ifstream filetext(fileText);
+    ofstream decrypt(fileDecrypt);
+    ifstream PK(fileKey);
+     if(!PK){
+        cout<<"Fail!publicKey.txt isn't exist";
+        return;
+    }
+    if(!filetext){
+        cout<<"Fail!filetext.txt isn't exist";
+        return;
+    }
+    ZZZ n,d,code;
+    string a;
+    /*read key from fileKey*/
+    PK>>a; n=a;
+    PK>>a; d=a;
+    /*read text from fileText*/
+    filetext>>a;
+    /*decrypto base64 to bits*/
+    code.set_str(de_convert_base64(a),2);
+    /*M=C^d mode n*/
+    mpz_powm(code.get_mpz_t(),code.get_mpz_t(),d.get_mpz_t(),n.get_mpz_t());
+    /*convert to bit and crop length of KEY{128,192,256}*/
+    a=code.get_str(2);
+    a=a.substr(2048-KEY,KEY);
+    /********************/
+    code.set_str(a,2);
+    decrypt<<code.get_str();
+
+    filetext.close();
+    decrypt.close();
+    PK.close();
+}
+void RSA::cryptRSA (char* fileMText,char* fileKey,char *fileCrypt)
+{
+    string headCode="1000010010000100";/*head16bit*/
+    ifstream filetext(fileMText);
+    ifstream filekey(fileKey);
+    ofstream filecrypt(fileCrypt);
+    if(!filetext){
+        cout<<"Fail!Filetext isn't exist";
+        return;
+    }
+    if(!filekey){
+        cout<<"Fail!Filekey isn't exist";
+        return;
+    }
+    /*read key from fileKey*/
+    string TEXT;
+    ZZZ n,e;
+    filekey>>TEXT;
+    n=TEXT;
+    filekey>>TEXT;
+    e=TEXT;
+    /*linked head16bit with bit random + KEY{128,192,256}*/
+    TEXT=headCode;
+    ZZ numbersite;
+    RandomBits(numbersite,(2032-KEY));
+    stringstream buffer;
+    buffer<<numbersite;
+    ZZZ code;
+    code=buffer.str();
+    int strlen=code.get_str(2).length();
+    for(;strlen<2032-KEY;strlen++)TEXT+='1';
+    TEXT+=code.get_str(2);
+    filetext>>headCode;
+    code=headCode;
+    TEXT+=code.get_str(2);
+    /*********************/
+    /*C=M^e mod n*/
+    code.set_str(TEXT,2);
+    mpz_powm(code.get_mpz_t(),code.get_mpz_t(),e.get_mpz_t(),n.get_mpz_t());
+    filecrypt<<convert_base64(code.get_str(2))<<endl;
+
+    filecrypt.close();
+    filetext.close();
+    filekey.close();
+}
+string RSA::convert_base64(const string &code)
+{
+    string decode;
+    ZZZ convert_number;
+    for(int i=0;i<code.length();i+=6)
+    {
+
+        convert_number.set_str(code.substr(i,6),2);
+        long ka=convert_number.get_ui();
+        if(ka<26) ka+=65;
+        else if(ka<52)ka+=71;
+        else if(ka<62)ka-=4;
+        else if(ka==62)ka=43;
+        else if(ka==63)ka=47;
+        decode+=(char)ka;
+    }
+    return decode;
+}
+string RSA::de_convert_base64(const string &code)
+{
+    string decode;
+    ZZZ convert_number;
+    long ka;
+    for(int i=0;i<code.length();i++)
+    {
+        ka=code[i];
+        if(ka<='Z'&& ka>='A') ka-=65;
+        else if(ka>='a'&&ka<='z')ka-=71;
+        else if(ka<='9'&&ka>='0')ka+=4;
+        else if(ka=='+')ka=62;
+        else if(ka=='/')ka=63;
+        convert_number=ka;
+        string ma=convert_number.get_str(2);
+        if(i<code.length()-1)
+        {
+            for(int j=0;j<6-ma.length();j++)
+            {
+                decode+='0';
+            }
+        }
+        decode+=ma;
+    }
+    return decode;
+}
+RSA::~RSA()
+{
+    //dtor
+}
